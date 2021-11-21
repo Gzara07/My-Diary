8fd4d82c148258bc08c170eaf3e962fbedf00cc7
STRIX in Notnik_kg <mamasydykova_g@auca.kg>
2021-11-22 00:42:33 +0600

    menu

--- app/src/main/java/com/example/mydiary/Login.kt
+++ app/src/main/java/com/example/mydiary/Login.kt
@@ -1,11 +1,30 @@
 package com.example.mydiary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val login2 = findViewById<Button>(R.id.login2)
+
+        login2.setOnClickListener {
+            startActivity(Intent(this, Account::class.java))
+            finish()
+        }
+        val sing_in = findViewById<TextView>(R.id.sing_in)
+
+        login2.setOnClickListener {
+            startActivity(Intent(this, Singin::class.java))
+            finish()
+        }
+
+
     }
 }
--- app/src/main/java/com/example/mydiary/Singin.kt
+++ app/src/main/java/com/example/mydiary/Singin.kt
@@ -1,11 +1,22 @@
 package com.example.mydiary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Singin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+
+
+        val sign_in = findViewById<Button>(R.id.sign_in)
+
+        sign_in.setOnClickListener {
+            startActivity(Intent(this, Account::class.java))
+            finish()
+        }
     }
 }
